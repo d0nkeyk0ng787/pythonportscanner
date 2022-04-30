74b8f2b6af969a0539db5d98ee3a988a23584d4d
Neb <77905135+d0nkeyk0ng787@users.noreply.github.com>
2022-04-30 14:55:26 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,71 @@
 #! /usr/bin/env python3
 
-import socket
+# TODO - Add multithreading / UNFUCK multithreading implementation
+# TODO - Fix the printing of the completed message
+
+import sys
 import time
+import socket
+import multiprocessing
 
 #address = input("IP: ")
 #port = input("Port: ")
 
-address = '127.0.0.1'
-aliveports = []
-timetaken = 0
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 start = time.time()
 
-with open('well-known-ports.txt', 'r') as f:
-    for i in f:
-        s_i = i.strip()
-        s_i = int(s_i)
-        error = False
-        
-        try:
-            s.connect((address, s_i))
-        except socket.error as e:
-            #print(e)
-            error = True
-            #print(f"Port {s_i} is down")
+def scanports(a, b):
 
-        if error == False:
-
+    address = '127.0.0.1'
+    aliveports = []
+    timetaken = 0
+    
+    try:
+        for i in range(a, b):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1)
+            res = s.connect_ex((address, i))
+            if res == 0:
+                aliveports.append(i)
+                print(f"Port {i} is alive")
             s.close()
-            
-            aliveports.append(s_i)
-            print(f"Port {s_i} is alive")
+
+    except socket.gaierror:
+        print("Hostname could not be reached")
+        sys.exit()
+    except socket.error:
+        print("Host unresponsive")
+        sys.exit()
+    except KeyboardInterrupt:
+        sys.exit()
 
     end = time.time()
     timetaken = (end - start)
-
     apcount = len(aliveports)
-    scandonemessage = (f"Portscan Completed | 1 IP Address {address} scanned | There are {apcount} alive ports | Scan completed in {timetaken} seconds")
-
+    scandonemessage = (f"Portscan Completed | 1 IP Address {address} scanned | There are {apcount} alive ports | Scan completed in {round(timetaken, 4)} seconds")
+    
     print(scandonemessage)
+    sys.exit()
+
+def finish():
+    pass
+
+
+if __name__ == "__main__":
+    
+    p1 = multiprocessing.Process(target=scanports, args= [1, 250])
+    p1.start()
+    p1.join()
+
+    p2 = multiprocessing.Process(target=scanports, args= [251, 500])
+    p2.start()
+    p2.join()
+
+    p3 = multiprocessing.Process(target=scanports, args = [501, 750])
+    p3.start()
+    p3.join()
+
+    p4 = multiprocessing.Process(target=scanports, args= [751, 1024])
+    p4.start()
+    p4.join()
+
     #print(aliveports)
